fa80d071a37264bd70abf294587e13639781b5ce
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-02-23 18:52:02 +0530

    fb data dedection added.

--- socialmodule/urls.py
+++ socialmodule/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
 from django.views.generic.base import TemplateView
 from socialmodule import views
-from socialmodule.views import ProfileView
+from socialmodule.views import ProfileView,DataDeletionView
 urlpatterns = [
     # path('', TemplateView.as_view(template_name='index.html')),
     # path('profile/',
     #      TemplateView.as_view(template_name='profile.html')),
      path('accounts/profile/',ProfileView.as_view() ),
+     path('deauthtication/',DataDeletionView.as_view(), name="deauthtication"),
     #  path('accounts/profile/',views.Profile,name="profile" )
 ]
--- socialmodule/views.py
+++ socialmodule/views.py
@@ -17,3 +17,52 @@ class ProfileView(LoginRequiredMixin, TemplateView):
 def Profile(request):
     return render(request, "profile.html")
 
+from django.utils.decorators import method_decorator
+
+import base64
+import hashlib
+import hmac
+from django.views.decorators.csrf import csrf_exempt
+from django.views.generic import View
+@method_decorator(csrf_exempt, name='dispatch')
+class DataDeletionView(View):
+    
+    def post(self, request, *args, **kwargs):
+        try:
+            signed_request = request.POST['signed_request']
+            encoded_sig, payload = signed_request.split('.')
+        except (ValueError, KeyError):
+            return HttpResponse(status=400, content='Invalid request')
+ 
+        try:
+            decoded_payload = base64.urlsafe_b64decode(payload + "==").decode('utf-8')
+            decoded_payload = json.loads(decoded_payload)
+ 
+            if type(decoded_payload) is not dict or 'user_id' not in decoded_payload.keys():
+                return HttpResponse(status=400, content='Invalid payload data')
+ 
+        except (ValueError, json.JSONDecodeError):
+            return HttpResponse(status=400, content='Could not decode payload')
+ 
+        try:
+            secret = 'your own app secret key'
+ 
+            sig = base64.urlsafe_b64decode(encoded_sig + "==")
+            expected_sig = hmac.new(bytes(secret, 'utf-8'), bytes(payload, 'utf-8'), hashlib.sha256)
+        except:
+            return HttpResponse(status=400, content='Could not decode signature')
+ 
+        if not hmac.compare_digest(expected_sig.digest(), sig):
+            return HttpResponse(status=400, content='Invalid request')
+ 
+        user_id = decoded_payload['user_id']
+ 
+        try:
+            # now you get facebook user id. you can delete its details from your database like below.
+            user_account = FacebookUserModel.objects.filter(fb_userid=user_id).delete()
+        except FacebookLoginDetails.DoesNotExist:
+            return HttpResponse(status=200)
+ 
+        # Own custom logic here
+ 
+        return HttpResponse(status=200)
