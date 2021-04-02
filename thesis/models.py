e0a91df99cfec1fa1310a322a0179fabf4cb9b0b
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-03 05:03:43 +0530

    Models created in the thesis.

---
+++ thesis/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+from allauth.socialaccount.models import SocialAccount
+# Create your models here.
+from cloudinary.models import CloudinaryField
+from cloudinary_storage.storage import RawMediaCloudinaryStorage
+
+# the uid given by the social account like google is smaller than Q so
+# we can use it as client secrete .
+class Senderkey(models.Model):
+    socialaccount =                      models.ForeignKey(SocialAccount,on_delete=models.CASCADE)
+    clientsecrete =                      models.CharField(max_length=50,default=None)
+    sks           =                      models.CharField(max_length=500)
+    pks           =                      models.CharField(max_length=500)
+
+
+
+
+class Receiverkey(models.Model):
+    socialaccount =                      models.ForeignKey(SocialAccount,on_delete=models.CASCADE)
+    clientsecrete =                      models.CharField(max_length=50,default=None)
+    sks           =                      models.CharField(max_length=500)
+    pks           =                      models.CharField(max_length=500)
+
+
+class Uploaddata(models.Model):
+    filename     =                       models.CharField(max_length=100)
+    datafile     =                       models.FileField(null = True)
+    words        =                       models.CharField(max_length=1000)
