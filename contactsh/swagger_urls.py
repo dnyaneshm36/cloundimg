dc0019ace075e4f8f9d6327208d3a58d1a22429b
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-05 01:36:04 +0530

    🦚🦚 Change doumetations url .🦚🦚

--- contactsh/swagger_urls.py
+++ contactsh/swagger_urls.py
@@ -20,7 +20,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-      url(r'^documentation', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+      url(r'^docs', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    url(r'^doc', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 
