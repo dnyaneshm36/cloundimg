1ed641b1d5a85c5e4061ae45670908df46ae54c6
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-05 00:23:23 +0530

    🐟🐟 Urls added to upload and show keys. 🐟🐟

--- cripto/urls.py
+++ cripto/urls.py
@@ -5,8 +5,12 @@ from django.conf.urls import url
 
 urlpatterns = [
      path("keys", views.KeysAPIView.as_view(),name="keyscreate"),
+     path("keygens", views.keygen,name="keygen"),
+     path("keyshow", views.Keydisplay,name="keyshow"),
      path("keys/<int:pk>", views.KeysDetailAPIView.as_view(),name="keysedit"),
-    
+     path('upload/', views.upload_file , name='upload'),
+     path('play/', views.CreateAPIViewkeys.as_view() , name='play'),
+     
 ]
 
 
