2453d6949a2aa34511ef6f864a7d098a11c03fe3
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-05 00:28:22 +0530

    ⛲⛲ Tried to add validations of file  for txt. ⛲⛲

--- thesis/form.py
+++ thesis/form.py
@@ -1,7 +1,17 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Uploaddata
 
-class UploaddataForm(ModelForm):
+class UploaddataForm(forms.ModelForm):
+    datafile        = forms.FileField()
     class Meta:
         model = Uploaddata
         fields = ['filename','datafile','words']
+    def clean_datafile(self, *args,**kwargs):
+        
+        datafile = self.cleaned_data.get("datafile")
+        strdata  = str(datafile)
+        if '.txt'in strdata:
+            return datafile
+        else:
+            raise forms.ValidationError(" Add text formate. ")
+        return datafile
