92201c5b184d8666f61b6eb92238cec20b1ee9e6
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-05 00:17:31 +0530

    🐢🐢 Debug ture fro testing. 🐢🐢

--- contactsh/settings.py
+++ contactsh/settings.py
@@ -25,7 +25,7 @@ import dj_database_url
 SECRET_KEY = os.getenv('SECRET_KEY', 'ABAC@GAIL.COM')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 # os.getenv('DEBUG', False)
 # print(DEBUG)
 # APPEND_SLASH = False
@@ -50,7 +50,7 @@ THIRD_PARTY_APPS = [
     'allauth.socialaccount.providers.facebook',
     'allauth.socialaccount.providers.google',
 
-    'rest_framework',
+     'rest_framework',
         'storages',
     'drf_yasg',
     
