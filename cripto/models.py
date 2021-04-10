b8acd324cfd947af52c116bae51eb838b2e14669
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-10 17:50:42 +0530

    🔱🔱  remove secreate key from model change to text ffields. 🔱🔱

--- cripto/models.py
+++ cripto/models.py
@@ -6,13 +6,10 @@ class Key(models.Model):
     clientId      =                      models.CharField(max_length=50,default=None)
     qu            =                      models.CharField(max_length=300)
     du            =                      models.CharField(max_length=300)
-    su            =                      models.CharField(max_length=100)
-    sku1          =                      models.CharField(max_length=100)
-    sku2          =                      models.CharField(max_length=300)
-    pku1          =                      models.CharField(max_length=300)
-    pku2          =                      models.CharField(max_length=300) 
+    pku1          =                      models.CharField(max_length=300,null=True)
+    pku2          =                      models.CharField(max_length=300,null=True) 
 
 class Uploadfile(models.Model):
-    userid          =                     models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    datafile      =                       models.FileField(null = True,upload_to='cripto/')
-    words         =                       models.CharField(max_length=1000)
+    userid          =                       models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    datafile        =                       models.FileField(null = True,upload_to='cripto/')
+    cypherwords     =                       models.TextField(max_length=10000,null=True)
