c71bd9ae02f0c58013807828c43200fbbfa3c215
dnyaneshm36 <dnyaneshm36@gmail.com>
2021-04-10 17:53:26 +0530

    ⛲⛲ Search  update and check  . ⛲⛲

--- cripto/urls.py
+++ cripto/urls.py
@@ -7,8 +7,12 @@ urlpatterns = [
      path("keys", views.KeysAPIView.as_view(),name="keyscreate"),
      path("keygens", views.keygen,name="keygen"),
      path("keyshow", views.Keydisplay,name="keyshow"),
+     path("publicupdate", views.uploadpublickey,name="uploadpublic"),
+     path("check", views.checkcerrect,name="correct"),
      path("keys/<int:pk>", views.KeysDetailAPIView.as_view(),name="keysedit"),
      path('upload/', views.upload_file , name='upload'),
+     path('search', views.trapdoorTest , name='search'),
+
      # path('play/', views.CreateAPIViewkeys.as_view() , name='play'),
      
 ]
